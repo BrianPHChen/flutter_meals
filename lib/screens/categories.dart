ba7cd6f73bfaa9f63c0f2480c1a33e3731d492b0
brianphchen <brian770130@gmail.com>
2024-08-12 23:32:26 -0700

    feat: modify folders name and add detail

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/data/dummy_data.dart';
-import 'package:meals/screen/meals.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/models/category.dart';
 
